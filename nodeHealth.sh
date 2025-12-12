73f9bec7ecc6c21a16bd817e8cc4c39e1f8b8a65
Pruthviraj@62 <mr.pruthviraj07@gmail.com>
2025-12-12 21:52:30 +0530

    nodehealth check script wrote..

---
+++ nodeHealth.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+######################################################### Author : Pruthviraj Patil
+# Date : 12/12/2025
+#
+# This Script outputs the node Health
+# Version : V1
+########################################################
+
+set -x  # Debug mode  
+set -e  # exist the script when there is an error 
+set -o  # pipefail (set -e will not errorout when there is pipefail so we have to use this set -o for pipefail)
+
+df -h
+
+free -g 
+
+nproc 
+
+ps -ef | grep amazon | awk -F" " '{print $2}' 
+
---
+++ test.sh
@@ -0,0 +1,6 @@
+echo 1
+echo 2
+echo 90
+echo 123
+echo 14
+
